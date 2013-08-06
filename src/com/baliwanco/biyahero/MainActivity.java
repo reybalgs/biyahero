40f278d179f63377aab38b5bf03b1a980eda7b87
Aldo Rey Balagulan <aldo_balagulan@lavabit.com>
2013-08-06 13:08:38 +0800

    MyRoutes activity added to manifest
    - Its parent is also now set to the MainActivity so the Up button works now
    - ListView also responds to taps made to the MyRoutes entry

--- src/com/baliwanco/biyahero/MainActivity.java
+++ src/com/baliwanco/biyahero/MainActivity.java
@@ -5,19 +5,21 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
 		Resources res = getResources();
 		ArrayList<String> list = new ArrayList<String>(Arrays.asList(
 				res.getStringArray(R.array.main_menu_items)));
@@ -30,12 +32,23 @@ public class MainActivity extends Activity {
 			Log.v("Main Activity", iterator.next());
 		}
 		
-		ListView main_list_view = (ListView) findViewById(R.id.main_list_view);
+		//ListView main_list_view = (ListView) findViewById(R.id.main_list_view);
 		// Set the adapter of the listview
-		main_list_view.setAdapter(adapter);
-		
+		//main_list_view.setAdapter(adapter);
+		setListAdapter(adapter);
 	}
 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		String item = (String) getListAdapter().getItem(position);
+		if(item.equals("My Routes")) {
+			// User has selected My Routes
+			Log.v("MainActivity", "User selected MyRoutes");
+			Intent intent = new Intent(this, MyRoutes.class);
+			startActivity(intent);
+		}
+	}
+ 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
