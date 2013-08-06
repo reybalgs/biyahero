78d0d51c906c02227b0cf8a0c9b09462e338dbad
Aldo Rey Balagulan <aldo_balagulan@lavabit.com>
2013-08-06 14:17:00 +0800

    Update Route class
    - Add Stop class for individual Stops

--- src/com/baliwanco/biyahero/model/Route.java
+++ src/com/baliwanco/biyahero/model/Route.java
@@ -1,9 +1,39 @@
 package com.baliwanco.biyahero.model;
 
+import java.util.ArrayList;
+
 public class Route {
 	/**
 	 * The Route class used for defining routes.
-	 * 
-	 * Nothing here as of now, though...
 	 */
+	private String name;
+	private String transit_type;
+	private double total_distance;
+	private ArrayList<Stop> stops;
+	
+	public String getTransit_type() {
+		return transit_type;
+	}
+	public void setTransit_type(String transit_type) {
+		this.transit_type = transit_type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getTotal_distance() {
+		return total_distance;
+	}
+	public void setTotal_distance(double total_distance) {
+		this.total_distance = total_distance;
+	}
+	public ArrayList<Stop> getStops() {
+		return stops;
+	}
+	public void setStops(ArrayList<Stop> stops) {
+		this.stops = stops;
+	}
+	
 }
---
+++ src/com/baliwanco/biyahero/model/Stop.java
@@ -0,0 +1,9 @@
+package com.baliwanco.biyahero.model;
+
+public class Stop {
+	/**
+	 * Class definition for a Stop. A Stop is somewhere where passengers can
+	 * alight and maybe board public transit. It can always be upgraded as a
+	 * terminal.
+	 */
+}
